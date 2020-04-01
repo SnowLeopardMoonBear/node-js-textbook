a6861dfebcd20d2fa0cb713168484fb92af165de
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-04-01 23:49:51 +0900

    add comments

--- chapter-04/how-to-send-html/htmlServer.js
+++ chapter-04/how-to-send-html/htmlServer.js
@@ -4,6 +4,7 @@ const fs = require("fs"); // read by using fs module of node.js
 http
   .createServer((req, res) => {
     fs.readFile("./requestedPage.html", (err, data) => {
+        //make it robust to errors because node.js is single-threaded and only one error could stop the entire server
       if (err) {
         throw err;
       }
